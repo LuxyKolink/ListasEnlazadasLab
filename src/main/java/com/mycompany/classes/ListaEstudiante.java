2e0168734bb9bd5768ac7efea835a14dc18e3ab2
santi <santi@LAPTOP-5DHNKV26>
2021-08-18 23:50:36 -0500

    Metodo print

--- src/main/java/com/mycompany/classes/ListaEstudiante.java
+++ src/main/java/com/mycompany/classes/ListaEstudiante.java
@@ -15,5 +15,35 @@ public class ListaEstudiante {
     private Node primero;
     private Node ultimo;
     public int cantidad;
+
+    public Node getPrimero() {
+        return primero;
+    }
+
+    public void setPrimero(Node primero) {
+        this.primero = primero;
+    }
+
+    public Node getUltimo() {
+        return ultimo;
+    }
+
+    public void setUltimo(Node ultimo) {
+        this.ultimo = ultimo;
+    }
     
+    
+    public void print() {
+        
+        Node current = getPrimero();
+
+        if (getPrimero() != null) {
+            do {
+                System.out.println("* Nombre:" + current.getValue().getNombre()+" /edad: "+current.getValue().getEdad());
+
+            } while (current != null);
+        } else {
+            System.out.println("Empty list!");
+        }
+    }
 }
